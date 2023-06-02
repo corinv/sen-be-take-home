c10301848f89dd4a44d985ba8059529259659947
corinv <corinvarney@gmail.com>
2023-06-02 15:52:21 +0100

    fix: add missing req param on root get controller

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Response } from "express";
+import express, { Response, Request } from "express";
 import { authenticate } from "../middleware/auth";
 import usersRouter from "./users.route";
 import authRouter from "./auth.route";
@@ -6,7 +6,7 @@ import postsRouter from "./posts.route";
 
 const router = express.Router({ mergeParams: true });
 
-router.get("", (res: Response) => res.sendStatus(200));
+router.get("", (_req: Request, res: Response) => res.sendStatus(200));
 router.use("/auth", authRouter);
 router.use("/users", authenticate, usersRouter);
 router.use("/posts", authenticate, postsRouter);
